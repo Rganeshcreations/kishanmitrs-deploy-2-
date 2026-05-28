3f7199397c5210fcf13a52eab33c70e239a264d3
Ganesh <gganesh4770@gmail.com>
2026-05-28 13:20:12 +0530

    Fixed Gmail OTP

--- backend/mailer.js
+++ backend/mailer.js
@@ -2,29 +2,25 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // TLS
+  service: "gmail",
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   }
-
 });
 
-// TEMPORARILY DISABLED FOR RAILWAY
-// transporter.verify((error, success) => {
+// Check transporter
+transporter.verify((error, success) => {
 
-//   if (error) {
-//     console.log("❌ Mail server connection failed");
-//     console.log(error);
-//   } 
-//   else {
-//     console.log("✅ Mail server ready");
-//   }
+  if (error) {
+    console.log("❌ Mail server connection failed");
+    console.log(error);
+  } 
+  else {
+    console.log("✅ Mail server ready");
+  }
 
-// });
+});
 
 module.exports = transporter;

1bb39635e0fb8d1100cc2d8fbcc5a718cfc3bce3
Ganesh <gganesh4770@gmail.com>
2026-05-28 15:52:05 +0530

    Added Brevo SMTP

--- backend/mailer.js
+++ backend/mailer.js
@@ -2,25 +2,22 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-
-  service: "gmail",
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   }
-
 });
 
 transporter.verify((error, success) => {
-
   if (error) {
     console.log("❌ Mail Error:", error);
-  } 
-  else {
+  } else {
     console.log("✅ Mail Server Ready");
   }
-
 });
 
 module.exports = transporter;
